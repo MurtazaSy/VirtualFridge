03e5c1bf9f69fff9313b77dbdd5b5f214a0331de
MurtazaSy <murataza.s@hotmail.de>
2015-04-26 16:30:28 +0200

    New Files
    TODO: Coloring DataGridView

---
+++ Kühlschrankwächter/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Kühlschrankwächter
+{
+    partial class VirtualFridge
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VirtualFridge));
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSearchreci = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.anzeigeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.schließenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.suchenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBoxSuchen = new System.Windows.Forms.ToolStripTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSave.Location = new System.Drawing.Point(12, 236);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(113, 39);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Speichern";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonSearchreci
+            // 
+            this.buttonSearchreci.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSearchreci.Location = new System.Drawing.Point(131, 236);
+            this.buttonSearchreci.Name = "buttonSearchreci";
+            this.buttonSearchreci.Size = new System.Drawing.Size(113, 39);
+            this.buttonSearchreci.TabIndex = 4;
+            this.buttonSearchreci.Text = "Rezept suchen";
+            this.buttonSearchreci.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(459, 203);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ablaufsdatum (MHD)";
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.Width = 200;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.anzeigeToolStripMenuItem,
+            this.suchenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(483, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schließenToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Menü";
+            // 
+            // anzeigeToolStripMenuItem
+            // 
+            this.anzeigeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.anzeigeToolStripMenuItem.Name = "anzeigeToolStripMenuItem";
+            this.anzeigeToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.anzeigeToolStripMenuItem.Text = "Anzeige";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Kein Filter",
+            "MHD < 7 Tage",
+            "MHD > 3 Tage",
+            "MHD < 3 Tage",
+            "MHD < 0 Tage"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // schließenToolStripMenuItem
+            // 
+            this.schließenToolStripMenuItem.Name = "schließenToolStripMenuItem";
+            this.schließenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.schließenToolStripMenuItem.Text = "Schließen";
+            this.schließenToolStripMenuItem.Click += new System.EventHandler(this.schließenToolStripMenuItem_Click);
+            // 
+            // suchenToolStripMenuItem
+            // 
+            this.suchenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBoxSuchen});
+            this.suchenToolStripMenuItem.Name = "suchenToolStripMenuItem";
+            this.suchenToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.suchenToolStripMenuItem.Text = "Suchen";
+            // 
+            // toolStripTextBoxSuchen
+            // 
+            this.toolStripTextBoxSuchen.Name = "toolStripTextBoxSuchen";
+            this.toolStripTextBoxSuchen.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBoxSuchen.Tag = "";
+            this.toolStripTextBoxSuchen.ToolTipText = "Produktnamen eingeben";
+            // 
+            // VirtualFridge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(483, 297);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSearchreci);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VirtualFridge";
+            this.Text = "Virtual Fridge";
+            this.Load += new System.EventHandler(this.VirtualFridge_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonSearchreci;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.ToolStripMenuItem anzeigeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem schließenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem suchenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxSuchen;
+    }
+}
+
---
+++ Kühlschrankwächter/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kühlschrankwächter
+{
+    public partial class VirtualFridge : Form
+    {
+        DateTimePicker dtpOrder;
+
+
+        public VirtualFridge()
+        {
+            InitializeComponent();
+        }
+        void Clear_all()
+        {
+            dataGridView1.Columns.Clear();
+        }
+
+        void ReadXML(String file)
+        {
+            DataSet myDataSet = new DataSet();
+            myDataSet.ReadXml(file);
+            dataGridView1.DataSource = myDataSet.Tables[0].DefaultView;
+        }
+
+
+        private DataTable GetDataTableFromDGV(DataGridView dgv)
+        {
+            var dt = new DataTable();
+            foreach (DataGridViewColumn column in dgv.Columns)
+            {
+                if (column.Index == 0 && column.Visible)
+                {
+                    // You could potentially name the column based on the DGV column name (beware of dupes)
+                    // or assign a type based on the data type of the data bound to this DGV column.
+                    dt.Columns.Add("Name");
+                }
+
+                if (column.Index == 1 && column.Visible)
+                {
+                    // You could potentially name the column based on the DGV column name (beware of dupes)
+                    // or assign a type based on the data type of the data bound to this DGV column.
+                    dt.Columns.Add("Ablaufdatum (MHD)");
+                }
+            }
+
+            object[] cellValues = new object[dgv.Columns.Count];
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+                dt.Rows.Add(cellValues);
+            }
+
+            return dt;
+        }
+        private void VirtualFridge_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("fridgeitems.xml"))
+            {
+                Clear_all();
+                ReadXML("fridgeitems.xml");
+
+            }
+
+            gridViewColoring();
+
+            toolStripComboBox1.SelectedIndex = 0;
+            dtpOrder = new DateTimePicker();
+            dtpOrder.Format = DateTimePickerFormat.Short;
+            dtpOrder.Visible = false;
+            dtpOrder.Width = 100;
+            dataGridView1.Controls.Add(dtpOrder);
+
+            dtpOrder.ValueChanged += this.dtpOrder_ValueChanged;
+            dataGridView1.CellBeginEdit += this.dataGridView1_CellBeginEdit;
+            dataGridView1.CellEndEdit += this.dataGridView1_CellEndEdit;
+        }
+
+        private void gridViewColoring()
+        {
+            dataGridView1.CurrentCell.Style.BackColor = Color.Gold;
+             switch (accessType)
+            {
+                case 1:
+                    chkReadOnly.Checked = true;
+                    break;
+                case 2:
+                    chkModify.Checked = true;
+                    break;
+            }
+        }
+    }
+           
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+                DataTable dT = GetDataTableFromDGV(dataGridView1);
+                DataSet dS = new DataSet();
+                dS.Tables.Add(dT);
+                File.Delete("fridgeitems.xml");
+                dS.WriteXml(File.OpenWrite("fridgeitems.xml"));
+                MessageBox.Show("Daten gespeichert.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+
+        }
+       
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            try
+            {
+                if ((dataGridView1.Focused) && (dataGridView1.CurrentCell.ColumnIndex == 1))
+                {
+                    dtpOrder.Location = dataGridView1.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, false).Location;
+                    dtpOrder.Visible = true;
+                    if (dataGridView1.CurrentCell.Value != DBNull.Value)
+                    {
+                        dtpOrder.Value = (DateTime)dataGridView1.CurrentCell.Value;
+                    }
+                    else
+                    {
+                        dtpOrder.Value = DateTime.Today;
+                    }
+                }
+                else
+                {
+                    dtpOrder.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            { 
+                //MessageBox.Show(ex.Message); 
+            }
+        }
+       
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if ((dataGridView1.Focused) && (dataGridView1.CurrentCell.ColumnIndex == 1))
+                {
+                    dataGridView1.CurrentCell.Value = dtpOrder.Value.Date;
+                }
+            }
+            catch (Exception ex)
+            { 
+                //MessageBox.Show(ex.Message); 
+            }
+        }
+        private void dtpOrder_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentCell.Value = dtpOrder.Text;
+        }
+
+        private void dateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void schließenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
+
+
